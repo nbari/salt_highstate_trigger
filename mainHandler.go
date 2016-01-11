5de200183ec4e519165d55dcf727def721d41cdb
nbari <nbari@dalmp.com>
2016-01-11 12:22:49 +0100

    modified:   mainHandler.go

--- mainHandler.go
+++ mainHandler.go
@@ -30,8 +30,7 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 	// We need to isolate the individual components of the path.
 	components := strings.Split(path, "/")
 
-	// added an * to match project_staging/production
-	salt_node := fmt.Sprintf("\"G@node_type:%s*\"", components[0])
+	salt_node := fmt.Sprintf("G@node_type:%s", components[0])
 
 	cmdName := "sudo"
 	cmdArgs := []string{"salt", "-C", salt_node, "state.highstate"}
